510e463ea4baf509f5a39463bda4f367a9348573
TendoProject <81413625+TendoProject@users.noreply.github.com>
2022-09-10 23:37:19 +0700

    Add files via upload

---
+++ src/ui/settings/general/language.ts
@@ -0,0 +1,16 @@
+export const language = {
+  general: 'General Settings',
+  generalMap: 'Map',
+  generalMapItems: 'Show Items',
+  generalMapPlayers: 'Show Players',
+  generalRadar: 'Radar',
+  generalRadarItems: 'Show Items',
+  generalRadarPlayers: 'Show Players',
+  generalSense: 'Sense',
+  generalSenseItems: 'Highlight Items',
+  generalSensePlayers: 'Highlight Players',
+  generalSenseLongRangeMode: 'Long Range Mode',
+  generalViewType: 'Main',
+  generalViewTypeMap: 'Show Map',
+  generalViewTypeRadar: 'Show Radar'
+};
